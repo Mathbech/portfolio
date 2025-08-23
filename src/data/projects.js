5d43c3a7bc09de182ae70f8741635cff1be0fa1c
Mathbech <legeek.you@gmail.com>
2025-08-23 19:07:46 +0200

    feat: ajout de nouvelles images et mise à jour des projets avec les couvertures correspondantes

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,34 +1,31 @@
+// Import des images depuis src/assets
+import itopoCover from '@/assets/images/itopo-cover.png'
+import beaupCover from '@/assets/images/beaup-cover.png'
+import filamentCover from '@/assets/images/filament-cover.png'
+
 export const featuredProjects = [
-    {
-      slug: 'filamentgestion',
-      title: 'FilamentGestion',
-      context: 'Projet scolaire/perso',
-      description: "Gestion de stocks pour impression 3D — en cours de commercialisation.",
-      cover: '/assets/images/filament-cover.jpg',
-      tech: ['Symfony', 'Docker', 'MariaDB']
-    },
-    {
-        slug: 'i-topo',
-        title: 'i-Topo',
-        context: 'Projet pro',
-        description: 'Application de gestion de chantiers, utilisée par des pros depuis 10+ ans.',
-        cover: '/assets/images/itopo-cover.jpg', // remplace/ajoute tes images
-        tech: ['Symfony', 'FLutter', 'AWS']
-    },
-    {
-        slug: 'multitaskbot',
-        title: 'MultitaskBot',
-        context: 'Projet personnel',
-        description: 'Un bot pour gérer plusieurs tâches sur Discord.',
-        cover: '/assets/images/multitaskbot-cover.jpg',
-        tech: ['Node.js', 'Discord.js', 'mysql']
-    },
-    {
-        slug: 'beaup-orientation',
-        title: "Beaup'orientation",
-        context: 'Projet scolaire',
-        description: "Web + mobile pour organiser des courses d’orientation géolocalisées.",
-        cover: '/assets/images/beaup-cover.jpg',
-        tech: ['Symfony', 'Flutter', 'API Platform', 'MySQL']
-    }
+  {
+    slug: 'i-topo',
+    title: 'i-Topo',
+    context: 'Projet pro',
+    description: 'Application de gestion de chantiers, utilisée par des pros depuis 10+ ans.',
+    cover: itopoCover,
+    tech: ['Symfony', 'Flutter', 'AWS']
+  },
+  {
+    slug: 'beaup-orientation',
+    title: "Beaup'orientation",
+    context: 'Projet scolaire',
+    description: "Web + mobile pour organiser des courses d’orientation géolocalisées.",
+    cover: beaupCover,
+    tech: ['Symfony', 'Flutter', 'MySQL', 'API Platform']
+  },
+  {
+    slug: 'filamentgestion',
+    title: 'FilamentGestion',
+    context: 'Projet cours/perso/pro',
+    description: "Gestion de stocks pour impression 3D — en cours de commercialisation.",
+    cover: filamentCover,
+    tech: ['Symfony', 'Docker', 'MariaDB']
+  }
 ]
