ad88a65b8f03fe5980abbdf334543512ca5a8f48
Mathbech <legeek.you@gmail.com>
2025-10-04 19:27:57 +0200

    Chore: add contact page with quick actions and styling

---
+++ src/data/contact.js
@@ -0,0 +1,25 @@
+// Données de config pour la page Contact (modifiable sans toucher aux composants)
+export const email = 'contact@bebech.fr';               // ← ton adresse
+export const defaultSubject = 'Contact depuis le portfolio';
+
+export const links = {
+  github: 'https://github.com/Mathbech',
+  linkedin: 'https://www.linkedin.com/in/mathieu-bechade/',
+  discord: 'https://discord.gg/6QjQ7N37zZ',
+};
+
+export const siteUrl = 'https://bebech.fr';
+export const person = {
+  firstName: 'Mathieu',
+  lastName: 'Bechade',
+};
+
+// Bloc projet
+export const filamentGestion = {
+  name: 'FilamentGestion',
+  tagline: "Application de gestion de stocks pour l'impression 3D",
+  description:
+    "Suivi des bobines, imprimantes et impressions. Projet en développement (Symfony & Twig). "
+  + "Des licences seront proposées à terme.",
+  siteUrl: 'https://filamentgestion.fr', // ← mets le bon domaine si différent
+};
