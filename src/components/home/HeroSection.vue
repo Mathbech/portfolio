2597f7bfa7a64c715bbde5b5caf45f10bf4f06fd
Mathbech <legeek.you@gmail.com>
2025-12-28 18:37:59 +0100

    Refactor/remove role variable from HeroSection.vue for clarity

--- src/components/home/HeroSection.vue
+++ src/components/home/HeroSection.vue
@@ -1,6 +1,5 @@
 <script setup>
 const name = 'Mathieu Béchade'
-const role = 'Développeur Web & Mobile'
 const punchline = 'Je conçois des applications fiables, claires et utiles, du concept à la mise en production.'
 </script>
 
